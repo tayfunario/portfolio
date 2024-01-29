810bb49f9f0bd8ff1378859bac9a0f7db17cc54e
tayfunetta <tayfunemrekahraman@gmail.com>
2024-01-29 17:56:23 +0300

    Add navigation link and back button to MainTitle component

--- components/project_detail/MainTitle.tsx
+++ components/project_detail/MainTitle.tsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { FaChevronLeft } from "react-icons/fa";
 
 const variants = {
   initial: { x: 1000 },
@@ -8,6 +10,9 @@ const variants = {
 export default function MainTitle({ title }: { title: string }) {
   return (
     <motion.div variants={variants} className="text-container px-4 py-2">
+      <Link href="/" className="flex items-center mb-1 text-xs text-cyan-600 font-semibold">
+        <FaChevronLeft className="size-[10px]" /> Geri dön
+      </Link>
       <h2 className="second-title">{title}</h2>
     </motion.div>
   );
