9931bdf8679c8839344a3b168f2cb33a118d0b55
tayfunetta <tayfunemrekahraman@gmail.com>
2024-02-05 22:20:01 +0300

    restyled down icon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({
   return (
     <div className="bg-gradient-to-r from-gradient-start to-gradient-end overflow-hidden">
       {pageNum !== 4 && (
-        <FaAngleDown className="absolute sm:bottom-2 bottom-1 right-1/2 translate-x-1/2 size-8 text-white" />
+        <FaAngleDown className="absolute sm:bottom-2 bottom-1 right-1/2 translate-x-1/2 size-7 bg-white text-sky-500 rounded-md z-50" />
       )}
       <div className="mx-auto max-w-screen-sm px-2">{children}</div>
       {pageNum && <PageIndicator pageNum={pageNum} />}
